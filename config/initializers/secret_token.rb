e6b4ce72eb7bc108c42b0b390d1fbbe4a5612753
David Fisher <david.fisher@gmail.com>
2011-01-10 11:34:52 -0500

    preset for test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galvitron::Application.config.secret_token = '19e4fbe5632c9d73f77306021ac51b8b65a57df20e8bee039fdf8ad2b4e92b157e3c4bbe6b769e438a1b55d6bd503beb1e032e359175a2252377950aac0742d5'
